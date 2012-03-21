d6596fe688433826a1dcd5f5e6f382869aec5789
Rahul Bansal <rahul.bansal@rtcamp.com>
2012-03-21 12:18:38 +0000

    cleaup

---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== rtPanel Hooks Editor ===
+Contributors: rtCamp 
+Donate link: http://rtpanel.com/donate/
+Tags: rtpanel, rtcamp, hooks, filters, actions
+Requires at least: 3.0
+Tested up to: 3.2.1
+Stable tag: 1.0.1
+
+This plugin adds hooks-editing interface in theme options for "rtPanel Theme Framework" - http://wordpress.org/extend/themes/rtpanel 
+
+== Description ==
+
+This plugin is add-on for [rtPanel Theme Framework](http://wordpress.org/extend/themes/rtpanel "rtPanel Theme Framework") and should be used alongwith it.
+
+It adds an option page under rtPanel theme options using which a user can write codes for various actions/filters hooks supported by rtPanel Theme Framework - [http://rtpanel.com/docs/developer/](http://rtpanel.com/docs/developer/ "rtPanel Hooks").
+
+Using this plugin a user need not edit any theme files to achieve desired functionality.
+
+= Useful Links =
+* [rtPanel Hooks Editor Plugin's Homepage](http://rtpanel.com/plugins/rtpanel-hooks-editor/)
+* [Support Forum](http://rtpanel.com/support/forum/plugins/rtpanel-hooks-editor/)
+
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+
+== Screenshots == 
+
+Please check ["rtPanel Hooks Editor" Plugin's Homepage](http://rtpanel.com/plugins/rtpanel-hooks-editor/).
+
+== Frequently Asked Questions ==
+
+= How can I find which hook to edit? =
+
+You can check [rtPanel hooks documentation here](http://rtpanel.com/docs/developer/).
+
+== Changelog ==
+
+= 1.0 =
+
+Initial release.
+
+= 1.0.1 =
+
+Escaping of textarea issue solved.
+
---
+++ rtpanel-hooks-editor.php
@@ -0,0 +1,249 @@
+<?php
+/*
+Plugin Name: rtPanel Hooks Editor
+Description: Apply hooks through rtPanel admin directly.
+Version: 1.0.1
+Author: rtCamp
+Author URI: http://rtcamp.com
+Contributors: rtCampers ( http://rtcamp.com/about/rtcampers/ )
+License: GNU General Public License, v2 (or newer)
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Setup default values on plugin activation
+register_activation_hook( __FILE__, 'default_values' );
+
+/** 
+ * Register rtPanel Hooks Editor with the Settings API
+ * 
+ * @since rtPanel Hooks Editor 1.0
+ */
+function rtp_register_hooks() {
+    register_setting( 'hook_settings', 'rtp_hooks', 'rtp_hooks_validate');
+}
+add_action( 'admin_init', 'rtp_register_hooks' );
+
+/** 
+ * Default Values
+ * 
+ * @since rtPanel Hooks Editor 1.0
+ */
+function default_values() {
+    $default_hooks = array(
+                        'before_header'          => '',
+                        'after_header'           => '',
+                        'before_logo'            => '',
+                        'after_logo'             => '',
+                        'begin_content_wrapper'  => '',
+                        'end_content_wrapper'    => '',
+                        'begin_content'          => '',
+                        'end_content'            => '',
+                        'begin_post'             => '',
+                        'end_post'               => '',
+                        'begin_post_title'       => '',
+                        'end_post_title'         => '',
+                        'post_meta_top'          => '',
+                        'post_meta_bottom'       => '',
+                        'begin_post_meta_top'    => '',
+                        'end_post_meta_top'      => '',
+                        'begin_post_meta_bottom' => '',
+                        'end_post_meta_bottom'   => '',
+                        'begin_post_content'     => '',
+                        'end_post_content'       => '',
+                        'begin_sidebar'          => '',
+                        'end_sidebar'            => '',
+                        'before_footer'          => '',
+                        'after_footer'           => '',
+                    );
+
+    if ( !get_option( 'rtp_hooks' ) ) {
+        update_option( 'rtp_hooks', $default_hooks );
+        $blog_users = get_users();
+
+        /* Set screen layout to 1 by default for all users */
+        foreach ( $blog_users as $blog_user ) {
+          $blog_user_id = $blog_user->ID;
+          if ( !get_user_meta( $blog_user_id, 'screen_layout_appearance_page_rtp_hooks' ) )
+          update_user_meta( $blog_user_id, 'screen_layout_appearance_page_hooks', 1, NULL );
+        }
+    }
+
+    return $default_hooks;
+}
+
+/** 
+ * Hook the rtPanel Hooks Editor to rtPanel
+ * 
+ * @since rtPanel Hooks Editor 1.0
+ */
+function rtp_hooks( $theme_pages ) {
+    $theme_pages['rtp_hooks'] = array(
+                                    'menu_title' => __( 'Hooks', 'rtPanel' ),
+                                    'menu_slug' => 'rtp_hooks'
+                                );
+    return $theme_pages;
+}
+add_filter( 'rtp_add_theme_pages', 'rtp_hooks' );
+
+/** 
+ * Validate the Hooks
+ * 
+ * @since rtPanel Hooks Editor 1.0
+ */
+function rtp_hooks_validate( $input ) {
+    if ( isset ( $_POST['rtp_reset'] ) ) {
+       $input = default_values();
+       add_settings_error( 'rtp_hooks', 'reset_rtp_hooks', __( 'All rtPanel Hooks have been restored to default.', 'rtPanel' ), 'updated' );
+    }
+    return $input;
+}
+
+/** 
+ * Contextual Help for rtPanel Hooks Editor
+ * 
+ * @since rtPanel Hooks Editor 1.0
+ */
+function rtp_hooks_screen_options() {
+    add_meta_box( 'hook_options', __( 'Hook Options', 'rtPanel' ), 'rtp_hooks_metabox', 'appearance_page_rtp_hooks', 'normal', 'core' );
+}
+add_action( 'rtp_hooks_metaboxes', 'rtp_hooks_screen_options' );
+
+function rtp_hook_help( $contextual_help, $screen_id, $screen ) {
+    $contextual_help = '<p>';
+    $contextual_help .= __( 'With rtPanel Hook Plugin you can write the code for all the action hooks available in rtPanel from you rtPanel Admin.', 'rtPanel' );
+    $contextual_help .= '</p><p>';
+    $contextual_help .= __( 'Have a look at all the hooks provided by rtPanel <a href="http://rtpanel.com/docs/developer/" title="rtPanel Hooks">here</a>.', 'rtPanel' );
+    $contextual_help .= '</p><p>';
+    $contextual_help .= __( '<strong>For more information, you can always visit:</strong>' , 'rtPanel' );
+    $contextual_help .= '</p><p>';
+    $contextual_help .= __( '<a href="http://rtpanel.com" title="rtPanel Official Page">rtPanel Official Page</a>' , 'rtPanel' );
+    $contextual_help .= '</p><p>';
+    $contextual_help .= __( '<a href="http://rtpanel.com/docs" title="rtPanel Documentation">rtPanel Documentation</a>' , 'rtPanel' );
+    $contextual_help .= '</p><p>';
+    $contextual_help .= __( '<a href="http://rtpanel.com/support" title="rtPanel Forum">rtPanel Forum</a>' , 'rtPanel' );
+    $contextual_help .= '</p>';
+    return $contextual_help;
+
+}
+add_filter( 'contextual_help', 'rtp_hook_help', 10, 3 );
+
+// Get the rtPanel Hooks Editor options from database
+$rtp_hooks = get_option( 'rtp_hooks' );
+
+/** 
+ * rtPanel Hooks Editor Page
+ * 
+ * @since rtPanelChild 1.0
+ */
+function rtp_hooks_options_page( $pagehook ) {
+    global $screen_layout_columns; ?>
+
+    <div class="options-main-container">
+        <?php settings_errors(); ?>
+        <a href="#" class="expand-collapse button-link" title="<?php _e( 'Show/Hide All', 'rtPanel' ); ?>"><?php _e( 'Show/Hide All', 'rtPanel' ); ?></a>
+        <div class="clear"></div>
+        <div class="options-container">
+            <form name="rt_hooks_form" id="rt_hooks_form" action="options.php" method="post" enctype="multipart/form-data">
+                <?php
+                /* nonce for security purpose */
+                wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false );
+                wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false ); ?>
+
+                <input type="hidden" name="action" value="save_rtp_metaboxes_hooks" />
+                <div id="poststuff" class="metabox-holder alignleft <?php echo 2 == $screen_layout_columns ? ' has-right-sidebar' : ''; ?>">
+                    <div id="side-info-column" class="inner-sidebar">
+                        <?php do_meta_boxes( $pagehook, 'side', '' ); ?>
+                    </div>
+                    <div id="post-body" class="has-sidebar">
+                        <div id="post-body-content" class="has-sidebar-content">
+                            <?php settings_fields( 'hook_settings' ); ?>
+                            <?php do_meta_boxes( $pagehook, 'normal', '' ); ?>
+                        </div>
+                    </div>
+                    <br class="clear"/>
+                    <input class="button-primary" value="<?php _e( 'Save All Changes', 'rtPanel' ); ?>" name="rtp_submit" type="submit" />
+                    <input class="button-link" value="<?php _e( 'Reset All Hooks Settings', 'rtPanel' ); ?>" name="rtp_reset" type="submit" />
+                </div>
+
+                <script type="text/javascript">
+                    //<![CDATA[
+                    jQuery(document).ready( function($) {
+                        // close postboxes that should be closed
+                        $('.if-js-closed').removeClass('if-js-closed').addClass('closed');
+                        // postboxes setup
+                        postboxes.add_postbox_toggles('<?php echo $pagehook; ?>');
+                    });
+                    //]]>
+                </script>
+            </form>
+        </div>
+    </div><?php
+}
+
+/** 
+ * rtPanel Hooks Editor Metaboxes ( Screen Options )
+ * 
+ * @since rtPanelChild 1.0
+ */
+function rtp_hooks_metabox() {
+        global $rtp_hooks; ?>
+            <br />
+            <strong><?php _e( 'Have a look at all the hooks available in rtPanel' ); ?> -> <a target="_blank" href="http://rtpanel.com/docs/developer/" title="rtPanel Hooks">http://rtpanel.com/docs/developer/</a></strong>
+            <br />
+            <br />
+            <strong><?php _e( 'Note', 'rtPanel' ); ?> : </strong><span class="description">Double Quotes(") should be escaped.</span>
+            <table class="form-table">
+                <tbody><?php
+                    $admin_hooks = default_values();
+                    $count = 1;
+                    foreach( $admin_hooks as $option_name => $value ) {
+                        $get_label = explode( '_', $option_name );
+                        $label = NULL;
+                        foreach( $get_label as $part_label ) {
+                            if ( $label )
+                                $label .= ' '.ucfirst( $part_label );
+                            else
+                                $label = ucfirst( $part_label );
+                        } ?>
+                        <tr valign="top">
+                            <th scope="row"><p><label for="<?php echo $option_name; ?>"><?php _e( $label, 'rtPanel' ); ?><br/><em>rtp_hooks_<?php echo $option_name;  ?></em></label></p></th>
+                            <td><textarea cols="33" rows="5" name="rtp_hooks[<?php echo $option_name; ?>]" id="<?php echo $option_name; ?>"><?php echo $rtp_hooks[$option_name]; ?></textarea><br /></td>
+                        </tr><?php
+                            if( !( $count % 2 ) ) { ?>
+                            <tr>
+                                <td colspan="2">
+                                    <div class="rtp_submit">
+                                        <input class="button-primary" value="<?php _e( 'Save All Changes', 'rtPanel' ); ?>" name="rtp_submit" type="submit" />
+                                        <input class="button-link" value="<?php _e( 'Reset All Hooks Settings', 'rtPanel' ); ?>" name="rtp_reset" type="submit" />
+                                        <div class="clear"></div>
+                                    </div>
+                                </td>
+                            </tr><?php
+                                } ?>
+                        <?php
+                        $count++;
+                    } ?>
+                </tbody>
+            </table>
+            <?php
+}
+
+/**
+ * Funtion to evalutate and execute php code
+ * 
+ * @since rtPanelChild 1.0
+ */
+function rtp_eval_php( $code ) {
+    ob_start();
+    eval("?>$code<?php ");
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;
+}
+
+/* Output the markup */
+foreach ( $rtp_hooks as $hook_name => $code ) {
+    if ( !empty( $code ) ) {
+        add_action( 'rtp_hook_'.$hook_name, create_function('', 'echo rtp_eval_php( "'.$code.'" );') );
+    }
+} ?>
